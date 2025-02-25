9b719f6e7088d7ce73aa49a7f2c2f47c6b0e0afa
AlexandreGoncalo <alexandrepedrogoncalo@gmail.com>
2025-02-25 16:13:29 +0000

    4 Commit

--- src/lib/nocors.js
+++ src/lib/nocors.js
@@ -1,14 +1,12 @@
 export function allowCors(handler) {
     return async (req, res) => {
-        // res.setHeader("Access-Control-Allow-Origin", "*");
-        // res.setHeader("Access-Control-Allow-Credential", "true");
-        res.setHeader('Access-Control-Allow-Origin', 'https://secondjs-mxvgm0rns-alexandregoncalos-projects.vercel.app');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Origin', 'https://secondjs-mxvgm0rns-alexandregoncalos-projects.vercel.app'); // Origem permitida
+        res.setHeader('Access-Control-Allow-Credentials', 'true'); // Corrigido
         res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        if(req.method === "OPTIONS") {
-            return res.status(200).end(); // Retorna 200 para todas as requisições
+        if (req.method === "OPTIONS") {
+            return res.status(200).end(); // Responder imediatamente a requisições OPTIONS
         }
 
         return handler(req, res);
